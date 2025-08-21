c0ec7f11671411c72f12981865dbd32e7ab168e3
Spectre <elouan.fare@protonmail.ch>
2025-08-21 17:57:27 +0200

    minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
+use std::io;
+
+use crate::level::{LevelDat, create_nbt};
+
 mod level;
 mod perlin;
 mod player;
 mod superflat;
 #[cfg(test)]
 mod test;
+
+pub fn create_level(path: &str, data: &LevelDat) -> Result<(), io::Error> {
+    create_nbt(data, path)
+}
